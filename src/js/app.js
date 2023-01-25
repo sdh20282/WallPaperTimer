1fbc3af7fe2fe4c59e6eacba2fb27ad51e33cbab
donghyeon.seo <sdh20282@gmail.com>
2023-01-25 22:01:53 +0900

    Feat : 타이머 형태 구현

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// const days = document.querySelector('#days');
+// const hours = document.querySelector('#hours');
+// const minutes = document.querySelector('#minutes');
+// const seconds = document.querySelector('#seconds');
+
