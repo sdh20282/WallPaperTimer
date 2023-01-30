ef6b7420ee2e968d957113fc111ac25c53efdb83
김주엽 <103459155+juye-ops@users.noreply.github.com>
2023-01-31 00:21:28 +0900

    Update app.js
    FEAT: Timer initialize

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
-// const days = document.querySelector('#days');
-// const hours = document.querySelector('#hours');
-// const minutes = document.querySelector('#minutes');
-// const seconds = document.querySelector('#seconds');
+var now = new Date()
+var birth = new Date("1998-9-7")
 
+var year_gap = now.getYear() - birth.getYear()
+var day_gap = now.getDay() - birth.getDay()
+var total_day = day_gap + year_gap * 365
+var hour_gap = now.getHours() - birth.getHours()
+var min_gap = now.getMinutes() - birth.getMinutes()
+var sec_gap = now.getSeconds() - birth.getSeconds()
+
+document.querySelector('#days').textContent = total_day;
+document.querySelector('#hours').textContent = hour_gap;
+document.querySelector('#minutes').textContent = min_gap;
+document.querySelector('#seconds').textContent = sec_gap;
